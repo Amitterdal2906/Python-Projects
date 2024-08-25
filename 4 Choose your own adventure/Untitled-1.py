89eb7e314fe3ed52fa6a5556005056cde4a87385
Amit Terdal <amitterdal2906@gmail.com>
2024-08-25 23:12:17 +0530

    Update Untitled-1.py

--- 4 Choose your own adventure/Untitled-1.py
+++ 4 Choose your own adventure/Untitled-1.py
@@ -1,8 +1,7 @@
 # Choose your own adventure
 
 
-name = input("Type your name: ")
-print("Welcome", name, "to this adventure!")
+
 
 answer = input(
     "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
